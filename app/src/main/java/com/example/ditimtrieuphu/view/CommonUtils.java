6cadc3fd12b5259dcc2f16b0b429956b335ad8e4
NamBin <nguyenhoainam.090801@gmail.com>
2021-05-03 21:42:18 +0700

    new update C1

--- app/src/main/java/com/example/ditimtrieuphu/view/CommonUtils.java
+++ app/src/main/java/com/example/ditimtrieuphu/view/CommonUtils.java
@@ -1,7 +1,11 @@
 package com.example.ditimtrieuphu.view;
 
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 public class CommonUtils {
+    private static final String FILE_NAME = "file_shared";
     private static CommonUtils instance;
     private CommonUtils() {
         //for singleton
@@ -14,4 +18,24 @@ public class CommonUtils {
         return instance;
     }
 
+    public boolean isExistPref(String key) {
+        SharedPreferences pref=App.getInstance().getSharedPreferences(FILE_NAME, Context.MODE_PRIVATE);
+        return pref.contains(key);
+    }
+
+    public void savePref(String key, String value) {
+        SharedPreferences pref=App.getInstance().getSharedPreferences(FILE_NAME,Context.MODE_PRIVATE);
+        pref.edit().putString(key,value).apply();
+    }
+
+    public String getPref(String key) {
+        SharedPreferences pref=App.getInstance().getSharedPreferences(FILE_NAME,Context.MODE_PRIVATE);
+        return pref.getString(key,null);
+    }
+
+    public void clearPref(String key) {
+        SharedPreferences pref = App.getInstance()
+                .getSharedPreferences(FILE_NAME, Context.MODE_PRIVATE);
+        pref.edit().remove(key).apply();
+    }
 }
